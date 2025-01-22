8214f2c0c90072704e3a0b8f92e8f5ddb7acad39
devklynch <devklynch@gmail.com>
2025-01-22 09:44:09 -0700

    added tests for sorting

--- spec/requests/api/v1/schedules_request_spec.rb
+++ spec/requests/api/v1/schedules_request_spec.rb
@@ -27,6 +27,14 @@ RSpec.describe "Schedules", type: :request do
         start_time: '12:00',
         end_time: '13:00')
 
+        @show3 = Show.create!(
+            artist: "Sabrina Carpenter",
+            location: "Gobi",
+            date: Date.new(2025, 4, 11),
+            start_time: '10:00',
+            end_time: '12:00'
+        )
+
         @schedule1 = Schedule.create!(
         title: "Devlin's Schedule",
         date: Date.new(2025, 4, 11),
@@ -38,7 +46,7 @@ RSpec.describe "Schedules", type: :request do
         user: @user12)
 
         @schedule1.shows.push(@show1)
-        @schedule2.shows.push(@show1,@show2)
+        @schedule2.shows.push(@show1,@show2, @show3)
     end
 
     describe "Index for all schedules" do
@@ -79,8 +87,48 @@ RSpec.describe "Schedules", type: :request do
             expect(json[:data][0][:attributes][:date]).to eq("2025-04-28")
             expect(json[:data][0][:attributes][:user_id]).to eq(@user12.id)
             expect(json[:data][0][:attributes][:shows]).to be_a(Array)
-            expect(json[:data][0][:attributes][:shows].count).to eq(2)
+            expect(json[:data][0][:attributes][:shows].count).to eq(3)
          end
+
+        it "can sort shows by artist A-Z" do
+            get "/api/v1/schedules/#{@schedule2.id}?sort_by=artist"
+
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(json[:data][0][:attributes][:shows][0][:artist]).to eq("Chappell Roan")
+            expect(json[:data][0][:attributes][:shows][1][:artist]).to eq("Sabrina Carpenter")
+            expect(json[:data][0][:attributes][:shows][2][:artist]).to eq("Tyler, The Creator")
+        end
+
+        it "can sort shows by artist Z-A" do
+            get "/api/v1/schedules/#{@schedule2.id}?sort_by=artist&order=desc"
+
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(json[:data][0][:attributes][:shows][2][:artist]).to eq("Chappell Roan")
+            expect(json[:data][0][:attributes][:shows][1][:artist]).to eq("Sabrina Carpenter")
+            expect(json[:data][0][:attributes][:shows][0][:artist]).to eq("Tyler, The Creator")
+        end
+
+        it "can sort shows by start times by earliest" do
+            get "/api/v1/schedules/#{@schedule2.id}?sort_by=start_time"
+
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(json[:data][0][:attributes][:shows][1][:artist]).to eq("Chappell Roan")
+            expect(json[:data][0][:attributes][:shows][0][:artist]).to eq("Sabrina Carpenter")
+            expect(json[:data][0][:attributes][:shows][2][:artist]).to eq("Tyler, The Creator")
+        end
+
+        it "can sort shows by start times by latest" do
+            get "/api/v1/schedules/#{@schedule2.id}?sort_by=start_time&order=desc"
+
+            json = JSON.parse(response.body, symbolize_names: true)
+
+            expect(json[:data][0][:attributes][:shows][1][:artist]).to eq("Chappell Roan")
+            expect(json[:data][0][:attributes][:shows][2][:artist]).to eq("Sabrina Carpenter")
+            expect(json[:data][0][:attributes][:shows][0][:artist]).to eq("Tyler, The Creator")
+        end
 
         it "returns an error if that schedule doesn't exist" do
             get "/api/v1/schedules/999999"
@@ -99,8 +147,9 @@ RSpec.describe "Schedules", type: :request do
             json = JSON.parse(response.body, symbolize_names: true)
         
             expect(response).to be_successful
-            expect(json[:data][0][:attributes][:shows].count).to eq(1)
+            expect(json[:data][0][:attributes][:shows].count).to eq(2)
             expect(json[:data][0][:attributes][:shows][0][:artist]).to eq("Chappell Roan")
+            expect(json[:data][0][:attributes][:shows][1][:artist]).to eq("Sabrina Carpenter")
         end
 
         it "returns an error with an invalid schedule" do
